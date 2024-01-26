bf50299bc761572d9bb8f89e6ad9e8bfe463a667
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-25 19:32:13 -0500

    Added Logo

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Nav = () => {
   return (
     <div className='h-[13vh] bg-gray-800'>
-        
+        <div className='w-[95%] md:w-[80%] mx-auto h-[100%] flex items-center justify-between'>
+            <div>
+                <Image src='/images/logo.png' alt='logo' width={140} height={140}></Image>
+            </div>
+        </div>
     </div>
   )
 }
