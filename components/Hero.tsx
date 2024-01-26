ef5aa31c5d45d4a349388c755122028730e3bb91
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-25 23:22:56 -0500

    Hero setup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative w-[100vw] h-[60vh] md:h-[87vh] bg'>
+        <div className='w-[80%] mx-auto h-[100%] flex flex-col items-start justify-center'>
+            
+        </div>
+    </div>
   )
 }
 
