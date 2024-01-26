7121c235df55b407f379e935747f536b8dcfec63
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-26 00:27:22 -0500

    Created header for FeaturedProducts

--- components/FeaturedProduct.tsx
+++ components/FeaturedProduct.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const FeaturedProduct = () => {
   return (
-    <div>FeaturedProduct</div>
+    <div className='pt-[6rem] pb-[3rem] bg-gray-900'>
+        <div className="w-[80%] mx-auto flex items-center justify-between">
+            <h1 className='md:text-[20px] test-[22px] lg:text-[34px] text-white'>
+                Featured Products
+            </h1>
+            <button className='uppercase text-[13px] md:text-[15px] text-indigo-300'>
+                View all products
+            </button> 
+        </div>
+    </div>
   )
 }
 
