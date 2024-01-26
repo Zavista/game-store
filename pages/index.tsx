0fc7b4b77e8a27eea6af54875f63edd87164da1d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-25 23:00:23 -0500

    Import Hero Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Hero from '../components/Hero'
 
 const HomePage = () => {
   return (
     <div className='overflow-x-hidden'>
       <Nav></Nav>
+      <Hero></Hero>
     </div>
   )
 }
