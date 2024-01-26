1b026c1175bb7c88a39a4d44b25ead90c5e1015d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-26 00:08:14 -0500

    Update Nav width

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,7 +5,7 @@ import {  HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon, UserIcon } from '@her
 const Nav = () => {
   return (
     <div className='h-[13vh] bg-gray-800'>
-        <div className='w-[95%] md:w-[80%] mx-auto h-[100%] flex items-center justify-between'>
+        <div className='w-[95%] mx-auto h-[100%] flex items-center justify-between'>
             <div>
                 <Image src='/images/logo.png' alt='logo' width={140} height={140}></Image>
             </div>
