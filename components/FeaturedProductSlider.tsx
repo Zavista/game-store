5ec00ffc934671a337cf44d9be83b663286b502e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-03 19:26:10 -0500

    Import Carousal and setup

--- components/FeaturedProductSlider.tsx
+++ components/FeaturedProductSlider.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Carousel from "react-multi-carousel";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1300 },
+    items: 3,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1300, min: 764 },
+    items: 3,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
 
 const FeaturedProductSlider = () => {
   return (
-    <div>FeaturedProductSlider</div>
+    <Carousel>
+      
+    </Carousel>
   )
 }
 
