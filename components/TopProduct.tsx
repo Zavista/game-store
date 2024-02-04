1872f03a0d16ca9eb50ac27ad5375d4db883874c
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-03 21:54:03 -0500

    Create header for Top product

--- components/TopProduct.tsx
+++ components/TopProduct.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const TopProduct = () => {
   return (
-    <div>TopProduct</div>
+    <div className='bg-gray-800 pt-[4rem] pb-[3rem]'>
+        <div className='w-[80%] mx-auto flex items-center justify-between'>
+            <h1 className='md:text-[20px] text-[22px] lg:text-[34px] text-white'>Top Products</h1>
+            <button className='text-[13px] md:text-[15px] text-stone-400' style={{ letterSpacing: '0.05em' }}>
+                View All Products
+            </button>
+        </div>
+    </div>
   )
 }
 
